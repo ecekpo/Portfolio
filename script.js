5a91ca6d4add01673a74d786529b56e8dd213700
Zedd <102124170+0zedd@users.noreply.github.com>
2022-06-25 10:33:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Mobile menu
-const hamburger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobile-menu');
+
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
