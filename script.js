12d29a572fbc9832a8ef5133d7d62623d53aa2c6
ecekpo <ememekpo@yahoo.com>
2022-07-11 21:06:44 +0100

    Add modified portfolio

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// Mobile menu
-const hamburger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobile-menu');
+// mobile menu section
+const mobileMenu = document.querySelector('.m-menu-bar');
+const hamburgerMenu = document.querySelector('.hamburger-menu-bar');
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
   mobileMenu.classList.toggle('active');
 });
 
-document.querySelectorAll('.menu-item').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
+document.querySelectorAll('.menu-list-1').forEach((n) => n.addEventListener('click', () => {
+  hamburgerMenu.classList.remove('active');
   mobileMenu.classList.remove('active');
 }));
 
-// Project Card Data
-const datacards = [
+// Portfolio card detail information
+const cards = [
   {
-    project_img: 'assest/pic1.PNG',
-    project_title: 'Facebook 360',
-    project_info_title: 'CANOPY',
-    counter_image: 'assest/dot.svg',
-    project_info_text: ['Back End Dev', '2015'],
+    card_image: 'assest/pic1.PNG',
+    card_title: 'Tonic',
+    card_title_item: 'CANOPY',
+    counter_image: 'assest/count-icon.PNG',
+    card_title_text: ['Back End Dev', '2015'],
     project_description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     tags: ['html', 'css', 'javascript'],
@@ -27,11 +27,11 @@ const datacards = [
     source_code: 'https://github.com/ecekpo/Portfolio.git',
   },
   {
-    project_img: 'assest/pic2.PNG',
-    project_title: 'Facebook 360',
-    project_info_title: 'CANOPY',
-    counter_image: 'assest/dot.svg',
-    project_info_text: ['Back End Dev', '2015'],
+    card_image: 'assest/pic2.PNG',
+    card_title: 'Multi-Post Stories',
+    card_title_item: 'CANOPY',
+    counter_image: 'assest/count-icon.PNG',
+    card_title_text: ['Back End Dev', '2015'],
     project_description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     tags: ['html', 'css', 'javascript'],
@@ -39,23 +39,23 @@ const datacards = [
     source_code: 'https://github.com/ecekpo/Portfolio.git',
   },
   {
-    project_img: 'assest/pic3.PNG',
-    project_title: 'Facebook 360',
-    project_info_title: 'CANOPY',
-    counter_image: 'assest/dot.svg',
-    project_info_text: ['Back End Dev', '2015'],
+    card_image: 'assest/pic3.PNG',
+    card_title: 'Facebook 360',
+    card_title_item: 'CANOPY',
+    counter_image: 'assest/count-icon.PNG',
+    card_title_text: ['Back End Dev', '2015'],
     project_description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     tags: ['html', 'css', 'javascript'],
     live_version: 'https://github.com/ecekpo/Portfolio.git',
     source_code: 'https://github.com/ecekpo/Portfolio.git',
   },
   {
-    project_img: 'assest/pic4.PNG',
-    project_title: 'Facebook 360',
-    project_info_title: 'CANOPY',
-    counter_image: 'assest/dot.svg',
-    project_info_text: ['Back End Dev', '2015'],
+    card_image: 'assest/pic4.PNG',
+    card_title: 'Uber Navigation',
+    card_title_item: 'CANOPY',
+    counter_image: 'assest/count-icon.PNG',
+    card_title_text: ['Back End Dev', '2015'],
     project_description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     tags: ['html', 'css', 'javascript'],
@@ -64,43 +64,43 @@ const datacards = [
   },
 ];
 
-// Populate Project Card using an array of project data objects
-const portfolio = document.querySelector('.works');
+// Section handles Card using an array of project data objects
+const portfolio = document.querySelector('.portfolio-works');
 portfolio.removeChild(portfolio.firstElementChild);
 
-datacards.forEach((project) => {
+cards.forEach((project) => {
   const ul = document.createElement('ul');
   ul.className = 'project-card';
   portfolio.appendChild(ul);
 
   let li = document.createElement('li');
   li.className = 'project-img-wrapper';
-  li.innerHTML = `<img src=${project.project_img} alt=${project.project_title} class='Project-img'>`;
+  li.innerHTML = `<img src=${project.card_image} alt=${project.card_title} class='Project-img'>`;
   ul.appendChild(li);
 
   li = document.createElement('li');
   li.className = 'project-content-block';
   ul.appendChild(li);
 
-  const projectHeader = document.createElement('div');
-  projectHeader.className = 'project-header';
-  li.appendChild(projectHeader);
+  const projectContainer = document.createElement('div');
+  projectContainer.className = 'project-container';
+  li.appendChild(projectContainer);
 
   const h1 = document.createElement('h1');
   h1.className = 'project-title';
-  h1.innerHTML = project.project_title;
-  projectHeader.appendChild(h1);
+  h1.innerHTML = project.card_title;
+  projectContainer.appendChild(h1);
 
   const projectInfo = document.createElement('div');
   projectInfo.className = 'project-info';
-  projectHeader.appendChild(projectInfo);
+  projectContainer.appendChild(projectInfo);
 
   const h2 = document.createElement('h2');
   h2.className = 'project-info-title';
-  h2.innerHTML = project.project_info_title;
+  h2.innerHTML = project.card_title_item;
   projectInfo.appendChild(h2);
 
-  project.project_info_text.forEach((infoText) => {
+  project.card_title_text.forEach((infoText) => {
     const img = document.createElement('img');
     img.src = project.counter_image;
     img.alt = 'counter';
@@ -124,7 +124,7 @@ datacards.forEach((project) => {
 
   project.tags.forEach((tagText) => {
     const tagItem = document.createElement('li');
-    tagItem.className = 'tag-item';
+    tagItem.className = 'tag-content';
     tagItem.textContent = tagText;
     tags.appendChild(tagItem);
   });
@@ -141,22 +141,22 @@ datacards.forEach((project) => {
   actionContainer.appendChild(actionButton);
 });
 
-// Desktop and Mobile pop-up Toogle
+// Section cover desktop and mobile pop-up Toogle
 const seeProjectBtn = Array.from(document.querySelectorAll('.see-project'));
 const closeModalBtn = document.querySelector('.modal-close-button');
 const modal = document.querySelector('.modal-container');
 const body = document.querySelector('body');
 
-// Handle see project button click
+// Event handler on see project button click
 seeProjectBtn.forEach((button, index) => {
   button.addEventListener('click', () => {
     modal.style.display = 'flex';
     body.style.overflow = 'hidden';
 
-    // Populate Pop-up Modal
-    const modalData = datacards[index];
+    // Pop-up Modal
+    const modalData = cards[index];
     const modalTags = Array.from(
-      document.querySelectorAll('.works-modal .tag-item'),
+      document.querySelectorAll('.works-modal .tag-content'),
     );
     const modalCounterImg = Array.from(
       document.querySelectorAll('.works-modal .project-info img'),
@@ -165,19 +165,19 @@ seeProjectBtn.forEach((button, index) => {
       document.querySelectorAll('.works-modal .project-info-item'),
     );
 
-    document.querySelector('.works-modal .project-title').innerHTML = modalData.project_title;
-    document.querySelector('.works-modal .project-info-title').innerHTML = modalData.project_info_title;
-    document.querySelector('.works-modal .Project-img').src = modalData.project_img;
+    document.querySelector('.works-modal .project-title').innerHTML = modalData.card_title;
+    document.querySelector('.works-modal .project-info-title').innerHTML = modalData.card_title_item;
+    document.querySelector('.works-modal .Project-img').src = modalData.card_image;
     document.querySelector('.works-modal .project-description').innerHTML = modalData.project_description;
     document.querySelector('.works-modal .see-live').href = modalData.live_version;
     document.querySelector('.works-modal .see-source').href = modalData.source_code;
 
     modalCounterImg.forEach((infoImg) => {
       infoImg.src = modalData.counter_image;
-      infoImg.alt = modalData.project_title;
+      infoImg.alt = modalData.card_title;
     });
     infoItems.forEach((infoItem, idx) => {
-      infoItem.innerHTML = modalData.project_info_text[idx];
+      infoItem.innerHTML = modalData.card_title_text[idx];
     });
     modalTags.forEach((tag, idx) => {
       tag.innerHTML = modalData.tags[idx];
@@ -193,12 +193,13 @@ seeProjectBtn.forEach((button, index) => {
   });
 });
 
-// Handle modal close button click
+// Moodal close button click action
 closeModalBtn.addEventListener('click', () => {
   modal.style.display = 'none';
   body.style.overflow = 'auto';
 });
 
+//  Email input, validation and form storage section
 const formemail = document.querySelector('#form');
 const emailerror = document.querySelector('.emailerror');
 const userEmail = document.querySelector('.useremail');
